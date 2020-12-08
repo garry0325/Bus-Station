adf1d59e3e24b4fc1fcc9050e045e3ad7ec4dcfb
garry0325 <garry0325@gmail.com>
2020-12-09 00:57:51 +0800

    Add sequence indicator on the annotation of the maps.

--- Bus Station/MapRouteViewController.swift
+++ Bus Station/MapRouteViewController.swift
@@ -15,12 +15,14 @@ class MapRouteViewController: UIViewController {
 	
 	var currentStop: BusStop!
 	var routeSequence = [BusStopLiveStatus]()
-	
+	var mapAnnotations = [StopAnnotation]()
 	
 	@IBOutlet var closeButtonTrailingToSafeAreaConstraint: NSLayoutConstraint!
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
+		
+		mapView.delegate = self
 		
 		// put the close button in the center if large screen
 		if(self.view.frame.height > 750.0) {
@@ -28,21 +30,69 @@ class MapRouteViewController: UIViewController {
 			NSLayoutConstraint(item: closeButton!, attribute: .centerX, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .centerX, multiplier: 1.0, constant: 0.0).isActive = true
 		}
 		
+		mapView.showsUserLocation = true
 		constructRouteSequence()
         // Do any additional setup after loading the view.
     }
     
 	func constructRouteSequence() {
+		var routeLocations = [CLLocationCoordinate2D]()
+		mapAnnotations = []
+		
 		for stop in routeSequence {
-			let annotation = MKPointAnnotation()
-			annotation.title = stop.stopName
-			annotation.coordinate = stop.location.coordinate
-			mapView.addAnnotation(annotation)
+			let stopAnnotation = StopAnnotation(coordinate: stop.location.coordinate)
+			stopAnnotation.title = stop.stopName
+			stopAnnotation.glyphText = String(stop.stopSequence)
+			stopAnnotation.sequence = stop.stopSequence
+			
+			mapAnnotations.append(stopAnnotation)
+			routeLocations.append(stop.location.coordinate)
 			
 			if(stop.stopId == currentStop.stopId) {
-				mapView.setRegion(MKCoordinateRegion(center: stop.location.coordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0), animated: false)
+				mapView.setRegion(MKCoordinateRegion(center: stop.location.coordinate, latitudinalMeters: 2000.0, longitudinalMeters: 2000.0), animated: false)
 			}
 		}
+		
+		mapView.addAnnotations(mapAnnotations)
+		mapView.addOverlay(MKPolyline(coordinates: routeLocations, count: routeLocations.count))
 	}
 
+	@IBAction func closeMapRouteViewController(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
+	}
+}
+
+extension MapRouteViewController: MKMapViewDelegate {
+	func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+		let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+		polylineRenderer.strokeColor = .systemBlue
+		polylineRenderer.lineWidth = 5
+		
+		return polylineRenderer
+	}
+	
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+		if let temp = annotation as? StopAnnotation {
+			var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "Stop") as? MKMarkerAnnotationView
+			if(annotationView == nil) {
+				annotationView = MKMarkerAnnotationView(annotation: temp, reuseIdentifier: "Stop")
+				annotationView?.glyphText = String(format: "%d", temp.sequence ?? 0)
+				annotationView?.titleVisibility = .visible
+			}
+			return annotationView
+		} else {
+			return nil
+		}
+	}
+}
+
+class StopAnnotation: NSObject, MKAnnotation {
+	var coordinate: CLLocationCoordinate2D
+	var title: String?
+	var glyphText: String?
+	var sequence: Int?
+	
+	init(coordinate: CLLocationCoordinate2D) {
+		self.coordinate = coordinate
+	}
 }
