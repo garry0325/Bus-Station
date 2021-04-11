08a20798743e25e84350e90defeee0e7b15d497b
garry0325 <garry0325@gmail.com>
2021-04-11 15:39:29 +0800

    Fix geo notification monitoring radius.

--- Bus Station/ViewController.swift
+++ Bus Station/ViewController.swift
@@ -649,7 +649,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
 			
 			let coordinates = MRTStationsByLine[geoStationsIndex[geoStation]![0][0]][geoStationsIndex[geoStation]![0][1]][1] as! CLLocationCoordinate2D
-			let region = CLCircularRegion(center: coordinates, radius: 60.0, identifier: geoStation)
+            let region = CLCircularRegion(center: coordinates, radius: 40.0, identifier: geoStation)
 			
 			region.notifyOnEntry = true
 			region.notifyOnExit = false
